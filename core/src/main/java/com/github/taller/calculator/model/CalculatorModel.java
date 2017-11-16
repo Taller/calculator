1e446213d872ad295c13a0840fc12bf234259b91
Taller <clever@bk.ru>
2017-11-16 17:26:56 +0300

    Possible compose numbers. (#3)

---
+++ core/src/main/java/com/github/taller/calculator/model/CalculatorModel.java
@@ -0,0 +1,14 @@
+package com.github.taller.calculator.model;
+
+import com.github.taller.calculator.exports.Operation;
+
+import java.util.LinkedList;
+
+public class CalculatorModel {
+
+    private LinkedList<Operation> operations = new LinkedList<>();
+
+    private LinkedList<String> data = new LinkedList<>();
+
+
+}
--- core/src/main/java/com/github/taller/calculator/ui/CalculatorController.java
+++ core/src/main/java/com/github/taller/calculator/ui/CalculatorController.java
@@ -1,19 +1,73 @@
 package com.github.taller.calculator.ui;
 
 
+import com.github.taller.calculator.model.CalculatorModel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 public class CalculatorController {
 
-    @FXML
-    private Button digit_0;
+    private CalculatorModel model = new CalculatorModel();
+
+    private String currentData = null;
 
     @FXML
-    public void digit(MouseEvent event) {
-        System.out.println(((Button)event.getSource()).getText());
+    private Label DISPLAY;
+
+    @FXML
+    public void addDigit(MouseEvent event) {
+        String pressed = ((Button) event.getSource()).getText();
+
+        if ("•".equals(pressed)) {
+            pressed = ".";
+        }
+
+        System.out.println("addDigit " + pressed);
+
+        if (currentData == null) {
+            currentData = pressed;
+        } else {
+            currentData += pressed;
+        }
+
+        DISPLAY.setText(currentData);
+
+        System.out.println(currentData);
+    }
+
+    @FXML
+    public void removeDigit(MouseEvent event) {
+        System.out.println("removeDigit");
+
+        if (currentData == null || currentData.isEmpty()) {
+            return;
+        }
+
+        currentData = currentData.substring(0, currentData.length() - 1);
+        DISPLAY.setText(currentData);
+
+        System.out.println(currentData);
+    }
+
+    @FXML
+    public void clearData(MouseEvent event) {
+        System.out.println("clearData");
+
+        currentData = null;
+
+        DISPLAY.setText("0");
+    }
+
+    @FXML
+    public void addOperation(MouseEvent event) {
+        System.out.println("addOperation");
+        System.out.println(((Button) event.getSource()).getText());
     }
 
 
+    @FXML
+    void initialize() throws Exception {
+    }
 }
